f18a98135a928ac1e3ec0b537ad3ca0225991928
Lutfu24 <lutfuhm@code.edu.az>
2022-11-29 13:19:40 +0400

    class

---
+++ class/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace Class
+{
+internal class Program
+    {
+    static void Main(string[] args)
+        {
+            Student student = new Student("Lutfu", "Mammadov", "P327", 79, true);
+            Student student1 = new Student("Orxan", "Eliyev", "G543", 89, false);
+
+            student.AllValues();
+            student.Exam();
+            student1.AllValues();
+            student1.Exam();
+        }
+    }
+}
---
+++ class/Sttudent.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    public class Student
+    {
+        public string Name;
+        public string Surname;
+        public string Group;
+        public byte Point;
+        public bool isGraduated;
+
+        public Student(string name, string surname, string group, byte point, bool isgraduated)
+        {
+            Name = name;
+            Surname = surname;
+            Group = group;
+            Point = point;
+            isGraduated = isgraduated;
+        }
+
+        public void AllValues()
+        {
+            Console.Write(Name + " " + Surname + " " + Group + " " + Point + " ");
+
+            if (isGraduated==true)
+            {
+                Console.WriteLine("Bu telebe mezun olub");
+            }
+            else
+            {
+                Console.WriteLine("Bu telebe mezun olmayib");
+            }
+        }
+
+        public void Exam()
+        {
+            if (Point>80)
+            {
+                Console.WriteLine("Ikinci imtahana gire biler");
+            }
+            else
+            {
+                Console.WriteLine("Ikinci imtahana gire bilmez");
+            }
+        }
+    
+    }
+}
